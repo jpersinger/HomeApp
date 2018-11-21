80e57cf88a214aaafc62d25b93d2684383b4fb5c
Julie Persinger <julie.persinger@willowtreeapps.com>
2018-11-21 15:52:49 -0500

    Change color on timer

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div
-        style={{
-          fontFamily: "Quicksand",
-          fontSize: "4em",
-          textAlign: "center"
-        }}
-      >
-        Welcome Home!
-      </div>
-    );
-  }
-}
+const colors = ["#38D15A", "#030F3D", "#F46A41", "#41DFF4", "#3BC8DB"];
+
+const Home = () => {
+  const [colorIndex, setColorIndex] = useState(0);
+
+  setInterval(() => {
+    if (colorIndex === colors.length - 1) {
+      setColorIndex(0);
+    } else {
+      setColorIndex(colorIndex + 1);
+    }
+  }, 1000);
+
+  return (
+    <div
+      style={{
+        fontFamily: "Quicksand",
+        fontSize: "4em",
+        paddingTop: "1em",
+        textAlign: "center",
+        color: colors[colorIndex],
+        transition: "color 300ms ease"
+      }}
+    >
+      Welcome Home!
+    </div>
+  );
+};
+
+export default Home;
