a52877bc289e8c63e665d4fe52e5e0d907a48293
Julie Persinger <julie.persinger@willowtreeapps.com>
2018-11-30 15:47:18 -0500

    Second redis

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import Navigation from "./pages/navigation";
 import { setup } from "./services/redis";
 import store from "./services/redux";
 
 const App = () => {
-  // useEffect(() => {
-  //   setup();
-  // });
-  setup();
+  useEffect(() => {
+    setup();
+  });
 
   return (
     <Provider store={store}>
--- src/services/redis/index.ts
+++ src/services/redis/index.ts
@@ -7,13 +7,19 @@ let client: redis.RedisClient;
 export const setup = () => {
   console.log("setting up");
   if (process.env.REDISTOGO_URL) {
+    console.log("process env", process.env.REDISTOGO_URL);
     const rtg = url.parse(process.env.REDISTOGO_URL) || {};
+    console.log("rtg", rtg, rtg.hostname as ClientOpts);
     client = redis.createClient(rtg.port || "", rtg.hostname as ClientOpts);
+    console.log("client", client);
 
     const auth = rtg.auth || "";
+    console.log("auth", auth);
     client.auth(auth.split(":")[1]);
+    console.log("client again", client);
   } else {
     client = redis.createClient();
+    console.log("new client", client);
   }
 
   setInterval(() => {
