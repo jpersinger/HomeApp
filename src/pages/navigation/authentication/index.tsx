d52aae1f1c9919ac2646e81ff433b5aaa968c61c
Julie Persinger <julie.persinger@willowtreeapps.com>
2018-12-06 14:48:10 -0500

    Add button styles

--- src/pages/navigation/authentication/index.tsx
+++ src/pages/navigation/authentication/index.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import { css } from "react-emotion";
 import { GoogleLogin } from "react-google-login-component";
 import Modal from "../../../components/modal";
+import theme from "../../../components/theme";
+import { Headline1 } from "../../../components/typography";
 import { getId } from "../../../services/server/home";
+
+const loginClassName = css`
+  background-color: ${theme.colors.secondary};
+  font-family: Quicksand;
+  color: white;
+  font-size: 1.3em;
+  padding: 0.5em;
+`;
 
 const Login = () => {
   const [clientId, setClientId] = useState("");
@@ -18,14 +29,27 @@ const Login = () => {
       title="Login"
       close={() => {}}
       content={
-        <GoogleLogin
-          socialId={clientId}
-          scope="profile"
-          prompt="select_account"
-          fetchBasicProfile
-          responseHandler={loginHandler}
-          buttonText="Login with Google"
-        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "2em"
+          }}
+        >
+          {!!clientId ? (
+            <GoogleLogin
+              socialId={clientId}
+              className={loginClassName}
+              scope="profile"
+              prompt="select_account"
+              fetchBasicProfile
+              responseHandler={loginHandler}
+              buttonText="Login with Google"
+            />
+          ) : (
+            <Headline1>Fetching id</Headline1>
+          )}
+        </div>
       }
     />
   );
