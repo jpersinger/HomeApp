b329323f64e80e936407fc0fb973d1c189f732eb
Julie Persinger <julie.persinger@willowtreeapps.com>
2019-01-02 13:36:14 -0500

    Update manifest

--- src/services/server/constants.ts
+++ src/services/server/constants.ts
@@ -1,33 +1,33 @@
-// export const SERVER_URL = "https://kisby-home-app-server.herokuapp.com";
-export const SERVER_URL = "http://localhost:3001";
+export const SERVER_URL = 'https://kisby-home-app-server.herokuapp.com';
+// export const SERVER_URL = "http://localhost:3001";
 
-export const HOME_HASH = "/home";
-export const RECIPE_HASH = "/recipes";
-export const BUDGET_HASH = "/budget";
-export const GENERAL_BUDGET_HASH = "/general";
-export const MONTHLY_EXPENSES_HASH = "/monthlyexpenses";
-export const PIGGY_BANKS_HASH = "/piggybanks";
-export const INCOME_HASH = "/incomes";
-export const SETTINGS_HASH = "/settings";
-export const PERSONAL_GOALS = "/personalgoals";
+export const HOME_HASH = '/home';
+export const RECIPE_HASH = '/recipes';
+export const BUDGET_HASH = '/budget';
+export const GENERAL_BUDGET_HASH = '/general';
+export const MONTHLY_EXPENSES_HASH = '/monthlyexpenses';
+export const PIGGY_BANKS_HASH = '/piggybanks';
+export const INCOME_HASH = '/incomes';
+export const SETTINGS_HASH = '/settings';
+export const PERSONAL_GOALS = '/personalgoals';
 
-export const MESSAGES_URL = HOME_HASH + "/messages";
-export const BIBLE_URL = HOME_HASH + "/bible";
+export const MESSAGES_URL = HOME_HASH + '/messages';
+export const BIBLE_URL = HOME_HASH + '/bible';
 
 export const GENERAL_BUDGET_URL = BUDGET_HASH + GENERAL_BUDGET_HASH;
 export const MONTHLY_EXPENSES_URL = BUDGET_HASH + MONTHLY_EXPENSES_HASH;
 export const PIGGY_BANKS_URL = BUDGET_HASH + PIGGY_BANKS_HASH;
 export const INCOME_URL = BUDGET_HASH + INCOME_HASH;
 
-export const GENERAL_BUDGET_POST_URL = GENERAL_BUDGET_URL + "/bank";
-export const GENERAL_BUDGET_JULIE_CREDIT_URL = GENERAL_BUDGET_URL + "/julie";
-export const GENERAL_BUDGET_BRYAN_CREDIT_URL = GENERAL_BUDGET_URL + "/bryan";
+export const GENERAL_BUDGET_POST_URL = GENERAL_BUDGET_URL + '/bank';
+export const GENERAL_BUDGET_JULIE_CREDIT_URL = GENERAL_BUDGET_URL + '/julie';
+export const GENERAL_BUDGET_BRYAN_CREDIT_URL = GENERAL_BUDGET_URL + '/bryan';
 
-export const DISPLAY_NAME_URL = SETTINGS_HASH + "/displayname";
-export const TEXT_COLOR_URL = SETTINGS_HASH + "/textcolor";
-export const LINKED_EMAILS_URL = SETTINGS_HASH + "/linkedemails";
+export const DISPLAY_NAME_URL = SETTINGS_HASH + '/displayname';
+export const TEXT_COLOR_URL = SETTINGS_HASH + '/textcolor';
+export const LINKED_EMAILS_URL = SETTINGS_HASH + '/linkedemails';
 export const PUSH_NOTIFICATIONS_URL =
-  SETTINGS_HASH + "/pushnotificationsenabled";
+  SETTINGS_HASH + '/pushnotificationsenabled';
 
-export const UPDATE_PERSONAL_GOAL_URL = PERSONAL_GOALS + "/updategoal";
-export const ADD_PERSONAL_GOAL_URL = PERSONAL_GOALS + "/addgoal";
+export const UPDATE_PERSONAL_GOAL_URL = PERSONAL_GOALS + '/updategoal';
+export const ADD_PERSONAL_GOAL_URL = PERSONAL_GOALS + '/addgoal';
